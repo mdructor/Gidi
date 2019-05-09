5c2d17066e4e7f8ae15d07e01df5106cabe19ceb
Mason Dructor <masondructor@gmail.com>
2019-05-09 00:36:26 -0500

    More work towards skinning

--- Source/GidiLookAndFeel.h
+++ Source/GidiLookAndFeel.h
@@ -6,9 +6,24 @@ class GidiLookAndFeel : public LookAndFeel_V4
 {
     public:
 
+        const Colour primary = Colour::fromRGB(45, 45, 45);
+        const Colour secondary = Colour::fromRGB(80, 80, 80);
+        const Colour accent = Colour::fromRGB(25, 25, 25);
+
         GidiLookAndFeel() {
 
-            setColour(ResizableWindow::backgroundColourId, Colour::fromRGB(45, 45, 45));
+            setColour(ResizableWindow::backgroundColourId, primary);
+
+            setColour(ComboBox::backgroundColourId, secondary);
+            
+            setColour(PopupMenu::backgroundColourId, secondary);
+
+            setColour(TextEditor::backgroundColourId, secondary);
+
+            setColour(Slider::ColourIds::thumbColourId, accent);
+
+            setColour(Slider::rotarySliderFillColourId, secondary);
+            setColour(Slider::ColourIds::rotarySliderOutlineColourId, secondary);
 
         }
 
