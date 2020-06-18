85eef96f29662ce8a592ddc1c66efd4cd316a89b
Mason Dructor <masondructor@gmail.com>
2020-06-17 23:17:05 -0500

    Add copyright notice and license info to src files

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,11 +1,21 @@
+/* 
+    Copyright 2017-2020 Mason Dructor
+*/
 /*
-  ==============================================================================
+    This file is part of Gidi.
 
-    This file was auto-generated!
+    Gidi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    It contains the basic startup code for a JUCE application.
+    Gidi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-  ==============================================================================
+    You should have received a copy of the GNU General Public License
+    along with Gidi.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include "../JuceLibraryCode/JuceHeader.h"
